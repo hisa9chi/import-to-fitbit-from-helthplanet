8f44928f8b0c1c2b4da7202f243b737dd334c7de
Hisashi Iguchi <owner@HisashinoMacBook-Pro.local>
2018-07-25 22:56:08 +0900

    コメントコード削除

--- lib/common_util.rb
+++ lib/common_util.rb
@@ -4,10 +4,3 @@ require 'hashie'
 def load_yml_file( path )
   yaml = Hashie::Mash.load( path )
 end
-
-# require 'yaml'
-
-# # yml ファイルの path を指定して読み込む
-# def load_yml_file( path )
-#   yaml = YAML.load_file( path )
-# end
